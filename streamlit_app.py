43bf66b7339caec97043060e9c0e11f77ce4f936
meruki25 <43439333+meruki25@users.noreply.github.com>
2024-12-12 00:39:36 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈Aplikasi Untuk Data Science")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
