ca0f83310ad32df09c119152bc95c66e487d6bcd
meruki25 <43439333+meruki25@users.noreply.github.com>
2024-12-12 08:48:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,13 @@ import pandas as pd
 
 st.title("🎈Aplikasi Untuk Data Science")
 
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
 
+st.dataframe(df, use_container_width=True)
 
