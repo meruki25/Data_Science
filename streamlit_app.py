0a269e69d0a47de9f90d213dfa38585b299f0c55
meruki25 <43439333+meruki25@users.noreply.github.com>
2024-12-12 08:54:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,22 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import time
 
 st.title("🎈Aplikasi Untuk Data Science")
 
-#df = pd.read_csv("")
-st.button("Masukkan Dataset")
+# Buat tombol untuk memasukkan dataset
+uploaded_file = st.file_uploader("Pilih file dataset", type=["csv", "xlsx", "xls"])
+
+# Jika tombol ditekan, maka akan memasukkan dataset
+if uploaded_file is not None:
+    # Baca file dataset
+    if uploaded_file.name.endswith(".csv"):
+        df = pd.read_csv(uploaded_file)
+    elif uploaded_file.name.endswith(".xlsx") or uploaded_file.name.endswith(".xls"):
+        df = pd.read_excel(uploaded_file)
+    
+    # Tampilkan dataset
+    st.write("Dataset yang dipilih:")
+    st.write(df)
 
